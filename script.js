f16cc7fbe015f6a1101974784b67cd9fb7668aee
philybellearq-hash <philybellearq@gmail.com>
2025-10-13 09:55:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -32,8 +32,8 @@ const projects = [
     },
     {
         image: 'images/7.gif',
-        number: 'MA: KULTURWERKSTATT EUROPA',
-        details: 'Master thesis, design for a culture and language hub in Aachen, KIT, Prof. M. Morger, 2018 <br>Keywords: Building in context. Positive/Negative'
+        number: 'M1: KULTURWERKSTATT EUROPA',
+        details: 'Master thesis, design for a culture and language hub in Aachen,<br> KIT, Prof. M. Morger, 2018 <br>Keywords: Building in context. Positive/Negative'
     },
     {
         image: 'images/8.jpg',
@@ -42,13 +42,13 @@ const projects = [
     },
     {
         image: 'images/9.jpg',
-        number: 'MP: SUNSET BLVD',
-        details: 'Master thesis, design for an affordable housing mixed-use megastructure, KIT, Prof. M. Frohn, 2018 <br>Keywords: Design through filmtheory. Flexible Floorplans. Future adaptability'
+        number: 'M2: SUNSET BLVD',
+        details: 'Master thesis, design for an affordable housing mixed-use megastructure,<br> KIT, Prof. M. Frohn, 2018 <br>Keywords: Design through filmtheory. Flexible Floorplans. Future adaptability'
     },
     {
         image: 'images/10.jpg',
-        number: 'MC: OPEN CODES',
-        details: 'Design entry for an exhibition at ZKM Karlsruhe curated bei Peter Weibel, KIT, Prof. M. Frohn, 2018, in collaboration with Juan Manuel Salgado <br>Keywords: Concept through translation. Grid. Virtual space'
+        number: 'M3: OPEN CODES',
+        details: 'Design entry for an exhibition at ZKM Karlsruhe curated bei Peter Weibel,<br> KIT, Prof. M. Frohn, 2018, in collaboration with Juan Manuel Salgado <br>Keywords: Concept through translation. Grid. Virtual space'
     },
     {
         image: 'images/4.jpg',
